0be25d458df4e7ea93b5bc8e4e6f7f7cbfb5741f
Christian <Christian.Huff.DEV@gmail.com>
2021-10-18 16:04:48 +0000

    Move config template up in the folder hierarchy

--- expand-config.sh
+++ expand-config.sh
@@ -1,8 +1,8 @@
 # Create a copy of the config template and rename it
-cp src/config.tmpl.json src/config.json
+cp config.tmpl.json config.json
 # Read all environmental variables set in the current environment
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 # Replace the keys in the config with the values from the environmental variables (if a match is found)
-cat src/config.json | envsubst $EXISTING_VARS | tee src/config.json
+cat config.json | envsubst $EXISTING_VARS | tee config.json
 # Move the populated config into the public folder
-mv src/config.json public/config.json
+mv config.json public/config.json
